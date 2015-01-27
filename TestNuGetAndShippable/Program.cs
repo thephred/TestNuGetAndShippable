d8697808e6622a61f758338a74969141b2654049
Michael Goff <Michael.Goff@Quantum.com>
2015-01-27 09:13:08 -0800

    Add projects

---
+++ TestNuGetAndShippable/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace TestNuGetAndShippable
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("Test: {0}", JsonConvert.SerializeObject(new {test = 3}));
+		}
+	}
+}
