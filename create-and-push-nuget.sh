16ded8a41dfaa96b84b4b821f324aa29ce206f3d
Michael Goff <Michael.Goff@Quantum.com>
2015-01-29 14:35:49 -0800

    put nuget building into a script

---
+++ create-and-push-nuget.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rm -f *.nupkg
+sed -i '' 's/\[BUILD_NUMBER\]/'"$BUILD_NUMBER"'/' TestNuGetAndShippable.nuspec
+mono nuget.exe pack TestNuGetAndShippable.nuspec
+mono nuget.exe push TestNuGetAndShippable.*.nupkg $MYGET_APIKEY -Source https://www.myget.org/F/thephred-test/api/v2/package
